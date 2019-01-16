d6ea4557db792559abda0702a95bdfbf33d6ef3f
Meyniel Matthieu <matthieu.meyniel@gfi.fr>
2019-01-16 18:48:29 +0100

    custom permissions

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import permissions, generics
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -19,6 +20,6 @@ class SnippetList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
